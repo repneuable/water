42f5df6fd6948092703cb98c733e8c3e95f79c1c
repneuable <32113593+repneuable@users.noreply.github.com>
2017-09-24 20:29:46 -0400

    Update The_Sortable_Titles.js

--- J5/The_Sortable_Titles.js
+++ J5/The_Sortable_Titles.js
@@ -185,6 +185,25 @@
    }
  }
 
+//SEARCH BOX 1
+function myFunction0() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("search0");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("referenceTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
  function myFunction() {
    var input, filter, table, tr, td, i;
    input = document.getElementById("searchTopics");
