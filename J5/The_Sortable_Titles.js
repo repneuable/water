6218bedaca4b24ea5a9e2e54db54ee57dc2e0981
repneuable <32113593+repneuable@users.noreply.github.com>
2017-09-25 12:21:09 -0400

    Update The_Sortable_Titles.js

--- J5/The_Sortable_Titles.js
+++ J5/The_Sortable_Titles.js
@@ -249,7 +249,7 @@ function myFunctionZ() {
   table = document.getElementById("myTableZ");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName("td")[1];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
