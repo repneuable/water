108951fd9175d69e3aa70c59fbf218c95b0fd6d1
repneuable <32113593+repneuable@users.noreply.github.com>
2017-11-19 22:20:42 -0500

    Update home.js

--- J5/home.js
+++ J5/home.js
@@ -57,7 +57,20 @@
 function toggleCONVERTER() {
     document.getElementById("draggable1-alt").style.display = "inline-table";
     document.getElementById("draggable2-alt").style.display = "inline-table";
-    document.getElementById("draggable3-alt").style.display = "inline-table";
+    document.getElementById("draggable3-alt").style.display = "table-row";
+}
+
+
+function viewSource(){
+    var source = "<html>";
+    source += document.getElementsByTagName('html')[0].innerHTML;
+    source += "</html>";
+    source = source.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    source = "<pre>"+source+"</pre>";
+    sourceWindow = window.open('','Source of page','height=800,width=800,scrollbars=1,resizable=1');
+    sourceWindow.document.write(source);
+    sourceWindow.document.close();
+    if(window.focus) sourceWindow.focus();
 }
 
 
